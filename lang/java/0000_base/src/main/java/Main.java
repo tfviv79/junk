11bb205eeb1c7fa4da0f3d334d8a363a2de4faa7
tfviv79 <tfviv79+github@gmail.com>
2018-02-05 03:37:32 +0000

    add junit plugin

--- lang/java/0000_base/src/main/java/Main.java
+++ lang/java/0000_base/src/main/java/Main.java
@@ -1,5 +1,8 @@
 import java.util.Arrays;
 
+import lombok.Data;
+
+@Data
 public class Main {
     public static void main(String[] args) {
         System.out.println("args=" + Arrays.toString(args));
