c06434db95f1a02be6b65c26385fffceb55023ae
tfviv79 <tfviv79+github@gmail.com>
2018-02-13 03:21:28 +0000

    can build and bootRun

---
+++ lang/java/0000_base_spring/src/main/java/com/example/Application.java
@@ -0,0 +1,20 @@
+package com.example;
+
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+import org.springframework.stereotype.*;
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController
+@EnableAutoConfiguration
+class Application {
+	@RequestMapping("/")
+	public String home() {
+		return "Hello World!";
+	}
+
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(Application.class, args);
+	}
+}
