4cb4a8727c43822de33fe027867f2ffbc4f2c809
tfviv79 <tfviv79+github@gmail.com>
2018-05-16 03:21:56 +0000

    remake for fx data.

--- lang/python/0003_chainer/a005.py
+++ lang/python/0003_chainer/a005.py
@@ -3,86 +3,8 @@
 
 from base import *
 
-## http://hi-king.hatenablog.com/entry/2015/06/27/194630:w
-if False:
-    class SmallClassificationModel(Chain):
-        def __init__(self):
-            super(SmallClassificationModel, self).__init__(
-                fc1 = L.Linear(2,2)
-                )
-        def __call__(self, x):
-            h = self.fc1(x)
-            #print("x0: {}, h0: {}".format(x.data, h.data))
-            return h
-
-        def train(self, optimizer, x_data, y_data):
-            x = Variable(x_data.reshape(1, 2).astype(np.float32))
-            y = Variable(y_data.astype(np.int32))
-            h = self(x)
-            #print("h0: {}".format(h.data))
-            
-
-            self.cleargrads()
-            error = F.softmax_cross_entropy(h, y)
-
-            error.backward()
-            optimizer.update()
-
-            #print("x: {}  --> h_class: {}".format(x.data, h.data.argmax()))
-            #print("  h: {}".format(h.data))
-
-    class ClassificationModel(Chain):
-        def __init__(self):
-            super(ClassificationModel, self).__init__(
-                fc1 = L.Linear(2,2),
-                fc2 = L.Linear(2,2)
-                )
-        def __call__(self, x):
-            h = F.sigmoid(self.fc1(x))
-            y = self.fc2(h)
-            #print("x0: {}, h0: {}".format(x.data, h.data))
-            return y
-
-        def train(self, optimizer, x_data, y_data):
-            x = Variable(x_data.reshape(1, 2).astype(np.float32))
-            y = Variable(y_data.astype(np.int32))
-            h = self(x)
-
-            self.cleargrads()
-            error = F.softmax_cross_entropy(h, y)
-            error.backward()
-            optimizer.update()
-
-            #print("x: {}  --> h_class: {}".format(x.data, h.data.argmax()))
-            #print("  h: {}".format(h.data))
-
-    class RegressionModel(Chain):
-        def __init__(self):
-            super(RegressionModel, self).__init__(
-                fc1 = L.Linear(2,2),
-                fc2 = L.Linear(2,1)
-                )
-        def __call__(self, x):
-            h = F.sigmoid(self.fc1(x))
-            y = self.fc2(h)
-            #print("x0: {}, h0: {}".format(x.data, h.data))
-            return y
-
-        def train(self, optimizer, x_data, y_data):
-            x = Variable(x_data.reshape(1, 2).astype(np.float32))
-            y = Variable(y_data.reshape(1, 1).astype(np.float32))
-            h = self(x)
-
-            self.cleargrads()
-            error = F.mean_squared_error(h, y)
-            error.backward()
-            optimizer.update()
-
-            #print("x: {}  --> h_class: {}".format(x.data, h.data.argmax()))
-            #print("  h: {}   err: {}".format(h.data, error))
-
 class MLP(Chain):
-    n_input = 2
+    n_input = 9
     n_output = 1
     n_units = 5
 
@@ -106,7 +28,7 @@ class MLP(Chain):
         return h4
 
     def train(self, optimizer, x_data, y_data):
-        x = Variable(x_data.reshape(1, 2).astype(np.float32))
+        x = Variable(x_data.reshape(1, 9).astype(np.float32))
         y = Variable(y_data.reshape(1, 1).astype(np.float32))
         h = self(x)
 
@@ -116,16 +38,16 @@ class MLP(Chain):
         optimizer.update()
 
 def apply_model(model, x_data):
-    x = Variable(x_data.reshape(1, 2).astype(np.float32))
+    x = Variable(x_data.reshape(1, 9).astype(np.float32))
     h = model(x)
     return h
 
 def run(model, optimizer, data, n=100):
     optimizer.setup(model)
-    for i, o in data*n:
+    for i, o in data:
         model.train(optimizer, i, o)
 
-in_data = pd.read_csv("testdata/USD_JPY_move_avg.csv",
+in_data = pd.read_csv("testdata/USD_JPY_move_avg.min.csv",
     names=["date", "cur", "ask", "bid", "mv_ask", "mv_bid"
             , "p_date", "p_ask", "p_bid"],
     dtype={"ask":np.float32, "bid":np.float32
@@ -134,46 +56,41 @@ in_data = pd.read_csv("testdata/USD_JPY_move_avg.csv",
     parse_dates=["date","p_date"]
     )
 
-def r(data, mode=4, n=100):
-    if mode == 1:
-        model = SmallClassificationModel()
-    elif mode == 2:
-        model = ClassificationModel()
-    elif mode == 3:
-        model = RegressionModel()
-    elif mode == 4:
+def pdata(X, start, length):
+    tmp_DataSet_X = (np.array(X[start:start + length]).reshape((-1, 1)).astype(np.float32))
+    start = start + 1
+    tmp_DataSet_Y = (np.array(X[start + length - 1:start + length]).reshape((-1, 1)).astype(np.float32))
+    return tmp_DataSet_X, tmp_DataSet_Y
+
+def atrain(start, length):
+    x, t = pdata(in_data.ask, start, length)
+    return [(x, t)]
+
+def r(data, model=None, n=100):
+    if not model :
         model = MLP()
     optimizer = optimizers.MomentumSGD(lr=0.01, momentum=0.9)
     run(model, optimizer, data, n)
+    x_vals = []
+    t_vals = []
+    y_vals = []
     for i, o in data:
-        print("{}: {} {}".format(i, apply_model(model, i).data, o))
+        y = apply_model(model, i).data
+        t_vals.append(i)
+        x_vals.append(o)
+        y_vals.append(y)
+        print("chec: {} <--> {}".format(y, o))
+    #out_data(t_vals, x_vals, y_vals, len(t_vals))
     return model
-    
 
 
-data_and = [
-    [np.array([0,0]), np.array([0])], 
-    [np.array([0,1]), np.array([0])], 
-    [np.array([1,0]), np.array([0])], 
-    [np.array([1,1]), np.array([1])], 
-]
+def out_data(t, x_train, y, suffix):
+    plt.plot(t,y)
+    plt.plot(t,x_train)
+    plt.savefig("fig/a005_{0}.png".format(suffix))
 
-data_xor = [
-    [np.array([0,0]), np.array([0])], 
-    [np.array([0,1]), np.array([1])], 
-    [np.array([1,0]), np.array([1])], 
-    [np.array([1,1]), np.array([0])], 
-]
-
-
-def out_data(x_train, y):
-    print(np.roll(y.data, -10))
-    print(x_train)
-    else:
-        plt.plot(t[:N_train],np.roll(y.data,-presteps))
-        plt.plot(t[:N_train],x_train)
-        plt.savefig("fig/a003_{0}.png".format(presteps))
+def main(trainnum=5, md=None):
+    return r(atrain(0, 9), n=trainnum, model=md)
 
 if __name__ == "__main__":
-    optimizer = optimizers.MomentumSGD(lr=0.01, momentum=0.9)
-    r(1, data_xor)
+    main()
