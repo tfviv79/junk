02c86fbee6f379f1988613b14d53f2a5e53c2cc4
tfviv79 <tfviv79@gmail.com>
2019-08-02 12:45:58 +0900

    add powershell scripts

---
+++ lang/powershell/mybk.ps1
@@ -0,0 +1,37 @@
+Param(
+)
+
+function bk_file()
+    param (
+        [Parameter(Mandatory=$true)][string] $filename
+    )
+
+    if ( -not $(Test-Path "$filename")) {
+        return "NG"
+    }
+
+    if (-not $(Test-Path "$filename" -PathType Leaf)) {
+        return "NG"
+    }
+
+    $filedate=$(Get-ItemProperty $filename).LastWriteTime
+    $workDate=$filedaate.ToString("yyyy-MM-dd_HH-mm-ss")
+
+    $workDir=Split-Path $filename -Parent
+    $destDir="$workDir/00-Archive"
+    if (-not $(Test-Path "$destDir")) {
+        New-Item -ItemType Directory -Path "$destDir" | Out-Null
+    }
+    $bkFileBase = (Get-Item $filename).BaseName
+    $bkExt = (Get-Item $filename).Extention
+    $bkFile = "$destDir/$bkFileBase.$workDate$bkExt"
+
+    Copy-Item $filename $bkFile
+    return "OK"
+}
+
+For ($i=0; $i -lt $Args.Count; $i++) {
+    $fn = $Args[$i]
+    $rslt=bk_file $fn
+    echo "$fn --- $rslt"
+}
---
+++ lang/powershell/myworkdir.ps1
@@ -0,0 +1,37 @@
+Param (
+    [string] $desc
+    )
+
+$workDate = Get-Date -Format "yyyy-MM-dd"
+
+$tmplDir = "c:\template_directory"
+$destDir = $(Convert-Path .)
+$destDir = "$destDir/${workDate}_${desc}"
+$issimple = $TRUE
+
+
+if ( Test-Path "$destDir") {
+    exit
+}
+
+
+New-Item -ItemType Directory -Path "$destDir" | Out-Null
+
+if (!$issimple)) {
+    Get-Children -Recurse "$tmplDir" | sort | ForEach-Object -Process {
+        $fname = $_.FullName
+        $destFname = $fname.SubString($tmplDir.Length)
+        $destFname = $destFname -replace "yyyy", "$dest"
+        $destFname = $destFname -replace "dddd", "$workDate"
+
+        if (Test-Path $fname -PathType Container) {
+            if (-Not (Test-Path "$destDir/$destFname")) {
+                mkdir "$destDir/$destFname" | Out-Null
+            }
+        } else {
+            Copy-Item -Recurse "$fname" -Destination "$destDir/$destFname"
+        }
+    }
+}
+
+cd $destDir
